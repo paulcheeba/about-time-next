eb0d4f7f64f60c38edf33f281b3a3656bd3c744d
Paulcheeba <75949480+paulcheeba@users.noreply.github.com>
2025-08-25 23:54:39 -0600

    Update preloadTemplates.js

--- module/preloadTemplates.js
+++ module/preloadTemplates.js
@@ -6,5 +6,5 @@ export const preloadTemplates = async function () {
     `modules/${MODULE_ID}/templates/countDown.html`
   ];
   const paths = raw.map(p => foundry?.utils?.getRoute ? foundry.utils.getRoute(p) : p);
-  return loadTemplates(paths);
+  return foundry.applications.handlebars.loadTemplates(templatePaths);
 };
