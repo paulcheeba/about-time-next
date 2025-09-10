ca9dc48138521067d20f09b96287411d5c532a65
Paulcheeba <75949480+paulcheeba@users.noreply.github.com>
2025-09-09 19:33:04 -0600

    Update ElapsedTime.js

--- module/ElapsedTime.js
+++ module/ElapsedTime.js
@@ -3,7 +3,7 @@
 // v13.0.7.3+ patch: ensure doIn/doEvery produce numeric increment (seconds)
 // for reliable repeating reschedules; respect SC when available.
 
-import { FastPriorityQueue, Quentry } from "./FastPirorityQueue.js";
+import { FastPriorityQueue, Quentry } from "./FastPriorityQueue.js";
 import { PseudoClock, PseudoClockMessage, _addEvent } from "./PseudoClock.js";
 import { currentWorldTime, dateToTimestamp, intervalATtoSC, intervalSCtoAT, secondsFromATInterval, normalizeATInterval } from "./calendar/DateTime.js";
 import { MODULE_ID } from "./settings.js";
